b18294d42156da240d1235dd098f5ac948937f63
Olivier <olivier.gomb+github@gmail.com>
2022-05-20 17:00:35 +0200

    suite

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root to: 'lists#index'
+  get '/show', to: 'lists#search'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: [:destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,59 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require "open-uri"
+
+List.create(name: 'Top movies')
+List.create(name: 'All time favorites')
+
+url = 'https://tmdb.lewagon.com/movie/now_playing'
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)['results']
+movies.each do |i|
+  movie = Movie.new(
+    title: i['title'],
+    overview: i['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{i['poster_path']}",
+    rating: i['vote_average']
+  )
+  movie.save
+  Bookmark.new(comment: 'machin machin machin', movie_id: movie.id, list_id: 2)
+end
+
+url = 'https://tmdb.lewagon.com/movie/upcoming'
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)['results']
+movies.each do |i|
+  Movie.create(
+    title: i['title'],
+    overview: i['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{i['poster_path']}",
+    rating: i['vote_average']
+  )
+end
+
+url = 'https://tmdb.lewagon.com/movie/top_rated'
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)['results']
+movies.each do |i|
+  Movie.create(
+    title: i['title'],
+    overview: i['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{i['poster_path']}",
+    rating: i['vote_average']
+  )
+  Bookmark.new(comment: 'truc truc truc', movie_id: movie.id, list_id: 1)
+end
+
+url = 'https://tmdb.lewagon.com/movie/latest'
+movies_serialized = URI.open(url).read
+movies = JSON.parse(movies_serialized)['results']
+movies.each do |i|
+  Movie.create(
+    title: i['title'],
+    overview: i['overview'],
+    poster_url: "https://image.tmdb.org/t/p/original#{i['poster_path']}",
+    rating: i['vote_average']
+  )
+end
